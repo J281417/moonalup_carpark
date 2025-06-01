8b43545fb7a52ed3104052887a0b5b94ef503d9a
j281417 <j281417@tafe.wa.edu.au>
2025-06-02 05:25:11 +0800

    feat(carpark): added constructor and attributes

--- src/car_park.py
+++ src/car_park.py
@@ -1,3 +1,17 @@
 class CarPark:
-    pass # this just means nothing... it actually does nothing
-# do this for ALL the other classes!
+    def __init__(
+        self,
+        location,
+        capacity,
+        plates=None,
+        sensors=None,
+        displays=None
+    ):
+        self.location = location
+        self.sensors = sensors or []
+        self.capacity = capacity
+        self.plates = plates
+        self.displays = displays
+
+    def __str__(self):
+        return f'Car park at {self.location} with {self.capacity} bays'
