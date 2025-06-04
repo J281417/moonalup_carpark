049de2f9e39fa5fbc6ce4acda7baa38d0e3aa4e8
j281417 <j281417@tafe.wa.edu.au>
2025-06-04 18:46:46 +0800

    feat(tests): added sensor tests

--- src/sensor.py
+++ src/sensor.py
@@ -5,7 +5,7 @@ class Sensor(ABC):
     def __init__(
         self,
         id,
-        is_active = "",
+        is_active = False,
         car_park = None
     ):
         self.id = id
@@ -22,8 +22,8 @@ class Sensor(ABC):
         pass
     
     def _scan_plate(self):
-        return random.choice(self.car_park.plates)
-    
+        return 'FAKE-' + format(random.randint(0, 999), "03d")
+
     def detect_vehicle(self):
         plate = self._scan_plate()
         self.update_car_park(plate)
@@ -34,6 +34,9 @@ class EntrySensor(Sensor):
         print(f"Incoming 🚘 vehicle detected. Plate: {plate}")
 
 class ExitSensor(Sensor):
+    def _scan_plate(self):
+        return random.choice(self.car_park.plates)
+    
     def update_car_park(self, plate):
         self.car_park.remove_car(plate)
         print(f"Outgoing 🚗 vehicle detected. Plate: {plate}")
